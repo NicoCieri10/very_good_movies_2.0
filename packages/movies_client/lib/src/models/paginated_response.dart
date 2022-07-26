61cca6458606178a2f1508ef68e90d9b667a8e3a
NicoCieri10 <nicocieri16@gmail.com>
2022-07-25 23:20:19 -0300

    Models and Requests done!

--- packages/movies_client/lib/src/models/paginated_response.dart
+++ packages/movies_client/lib/src/models/paginated_response.dart
@@ -1,4 +1,4 @@
-import 'package:movies_client/src/models/movies_dates.dart';
+part of 'models.dart';
 
 /// {@template paginated_response}
 /// A generic class for paginated responses.
@@ -13,6 +13,42 @@ class PaginatedResponse<T> {
     this.dates,
     required this.results,
   });
+
+  /// Returns a new [PaginatedResponse] with the given [results].
+  factory PaginatedResponse.fromJson(
+      T Function(Map<String, dynamic> map) itemParser,
+      Map<String, dynamic> json) {
+    final dates = json['dates'] as Map<String, dynamic>?;
+
+    // make sure we're dealing with a JSON map
+    json = json.cast<String, dynamic>();
+
+    // extract the results from the map
+    late final List<Map<String, dynamic>> items;
+
+    final mapResults = json['results'];
+
+    if (mapResults is List) {
+      items = mapResults
+          .cast<Map<dynamic, dynamic>>()
+          .map((Map<dynamic, dynamic> item) => item.cast<String, dynamic>())
+          .toList();
+    } else if (mapResults is Map) {
+      items = [mapResults.cast<String, dynamic>()];
+    }
+
+    // parse the results into a list of objects
+    final parsedItems = items.map(itemParser).toList();
+
+    return PaginatedResponse(
+      id: json['id'] as int?,
+      results: parsedItems,
+      page: json['page'] as int? ?? 0,
+      totalPages: json['total_pages'] as int? ?? 0,
+      totalResults: json['total_results'] as int? ?? 0,
+      dates: dates == null ? null : MoviesDates.fromJson(dates),
+    );
+  }
 
   /// The id of the response.
   final int? id;
@@ -31,28 +67,4 @@ class PaginatedResponse<T> {
 
   /// The dates of the movies.
   final MoviesDates? dates;
-
-  /// Returns a new [PaginatedResponse] with the given [results].
-  factory PaginatedResponse.fromJson(T Function(Map<String, dynamic> map) itemParser, Map<String, dynamic> json) {
-    // make sure we're dealing with a JSON map
-    json = json.cast<String, dynamic>();
-
-    // extract the results from the map
-    late final List<Map<String, dynamic>> items;
-
-    final mapResults = json['results'];
-
-    if (mapResults is List) {
-      items = mapResults.cast<Map<dynamic, dynamic>>().map((Map<dynamic, dynamic> item) => item.cast<String, dynamic>()).toList();
-    } else if (mapResults is Map) {
-      items = [mapResults.cast<String, dynamic>()];
-    }
-
-    // parse the results into a list of objects
-    final parsedItems = items.map(itemParser).toList();
-
-    return PaginatedResponse(
-      results: parsedItems,
-    );
-  }
 }
